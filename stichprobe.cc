7e1ee2d9d68af4a20c0e222988b3245878789580
Malte <malte.molgedei@gmx.de>
2024-10-30 16:07:36 +0000

    Ausgabe 3

--- stichprobe.cc
+++ stichprobe.cc
@@ -13,4 +13,14 @@ for(int i = 0; i < 234; ++i){
  std::cout << summe << std::endl; 
   double mean = summe/234;
   std::cout << mean << std::endl;
+
+  double diff = 0;
+  std::ifstream fin2("datensumme.txt");
+  for(int i = 0; i < 234; ++i){
+    fin2>>a;
+    diff= diff + (a-mean)*(a-mean);
+  }
+  std::cout << diff/234 << std::endl;
+  fin.close();
+  fin2.close();
 }
